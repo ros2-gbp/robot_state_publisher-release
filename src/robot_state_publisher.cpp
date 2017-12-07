00a95a9bccaedcaf33ff2e4280443330e63e0f68
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-12-06 21:41:22 -0800

    Imported upstream version '1.13.4' of 'upstream'

--- src/robot_state_publisher.cpp
+++ src/robot_state_publisher.cpp
@@ -69,7 +69,7 @@ geometry_msgs::msg::TransformStamped kdlToTransform(const KDL::Frame & k)
 }
 
 RobotStatePublisher::RobotStatePublisher(
-  rclcpp::node::Node::SharedPtr node_handle,
+  rclcpp::Node::SharedPtr node_handle,
   const KDL::Tree & tree,
   const urdf::Model & model)
 : model_(model),
@@ -112,7 +112,8 @@ void RobotStatePublisher::addChildren(const KDL::SegmentMap::const_iterator segm
 
 
 // publish moving transforms
-void RobotStatePublisher::publishTransforms(const std::map<std::string, double> & joint_positions,
+void RobotStatePublisher::publishTransforms(
+  const std::map<std::string, double> & joint_positions,
   const builtin_interfaces::msg::Time & time,
   const std::string & tf_prefix)
 {
